67ea02a0c2c756c167bb1bc6e9d05d85d764578c
Pär Bohrarper <par.bohrarper@gmail.com>
2022-09-12 10:57:28 +0200

    Update script#

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+# Restart pulseaudio, setup loopback
 pulseaudio -k
 pulseaudio --start
 pactl load-module module-loopback latency_msec=1 source=1 sink=1
-SDL_VIDEODRIVER=x11 LD_PRELOAD=/usr/local/lib/libserialport.so.0 ./m8c
+
+# Run m8c
+SCRIPTDIR=$(cd "$(dirname "$0")" ; pwd -P)
+SDL_VIDEODRIVER=x11 LD_PRELOAD=/usr/local/lib/libserialport.so.0 "$SCRIPTDIR/m8c"
+
+# Restart pulseaudio again
+pulseaudio -k
+pulseaudio --start
